5ff23a2e1fb756156c12f1d661a9e2c48b7ae325
Jon Shaulis <jon@mscreations.net>
2026-07-23 00:35:10 -0400

    Add copyright notice and licensing information to all source files

--- internal/plugins/manifest.go
+++ internal/plugins/manifest.go
@@ -1,3 +1,18 @@
+// Copyright (C) 2026 Jon Shaulis
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 // Package plugins is the host-side client for hhq's external-process plugin
 // contract: each plugin is an independently-run HTTP service implementing
 // GET /manifest, GET /view, GET /events, GET+POST /settings, and
